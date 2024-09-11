7b3968e0961c530261e9a005c1dea3a108bc7299
Nathan Fabio <nathanfabio.dev@gmail.com>
2024-09-10 21:18:09 -0300

    add GET route

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -3,10 +3,21 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/nathanfabio/go-microservice/cmd/api/controllers"
+	"github.com/nathanfabio/go-microservice/internal/repositories"
 )
 
 func CategoryRoutes(router *gin.Engine) {
 	routes := router.Group("/categories")
 
-	routes.POST("/", controllers.CreateCategory)
+	inMemoryCategoryRepository := repositories.NewInMemoryCategoryRepository()
+	
+	routes.POST("/", func(ctx *gin.Context) {
+		controllers.CreateCategory(ctx, inMemoryCategoryRepository)
+
+	})
+
+	routes.GET("/", func(ctx *gin.Context) {
+		controllers.ListCategory(ctx, inMemoryCategoryRepository)
+		
+	})
 }
